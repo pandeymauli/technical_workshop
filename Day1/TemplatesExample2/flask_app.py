7e904a06dc7693af86c3ef8f202d68dfe9ebb305
Mauli Pandey <pandeymauli@0587337950.wireless.umich.net>
2018-04-30 15:04:08 -0400

    Example 2

---
+++ Day1/TemplatesExample2/flask_app.py
@@ -0,0 +1,18 @@
+from flask import Flask, request, render_template, url_for
+
+app = Flask(__name__)
+
+@app.route('/oranges')
+def lemons():
+    title = "My Ice Cream Form"
+    # Add code -- what type should options hold?
+    return render_template('seeform.html',title=title_var, lst_stuff=options)
+
+@app.route('/apples')
+def plants():
+    ## Add code here
+    return render_template('results.html',flavors=flavor_options, name_len=name_len, name=name)
+
+
+if __name__ == "__main__":
+    app.run()
